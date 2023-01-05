2bf66f6633e69592967fcf756bc71c1338f187e6
Dustin Specker <DustinSpecker@DustinSpecker.com>
2023-01-05 07:54:50 -0600

    feat(get-cover): demonstrate executing binary twice

--- get-cover.sh
+++ get-cover.sh
@@ -10,7 +10,15 @@ mkdir coverage/unit
 go test -cover  ./... -args -test.gocoverdir="$PWD/coverage/unit/"
 go build -cover -o ./bin/add ./cmd/add/
 
+# demonstrate running binary twice and creating multiple code coverage reports for integration tests
+# success
 GOCOVERDIR=coverage/int ./bin/add 1 3
+
+# will fail because no args provided
+if GOCOVERDIR=coverage/int ./bin/add ; then
+  echo "add should exit with non-zero exit code when no args provided"
+  exit 1
+fi
 
 # show coverage individually
 go tool covdata percent -i=./coverage/unit
